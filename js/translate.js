a4084f55015a02fdfd086fcfc9808bf345f91c00
Tim Hosgood <timhosgood@googlemail.com>
2019-09-17 23:44:20 +0200

    parsing now gives structured return

--- js/translate.js
+++ js/translate.js
@@ -14,12 +14,13 @@ translateInput.keyup(function() {
     return;
   }
 
-  var parsedInput = [];
+  var parsedInput = {};
   searchValue.split(' ').forEach(function(item) {
     if (item === '') {
       return;
     }
 
+    parsedInput[item] = {};
     var itemAdded = false;
 
     // var regex = new RegExp(item);
@@ -27,47 +28,21 @@ translateInput.keyup(function() {
       Object.keys(nouns[nHash].root).map(lang => {
         var atom = nouns[nHash].root[lang].atom;
         // if (atom.search(regex) != -1) {
-         if (atom === item) {
-          parsedInput.push(`<span class="noun ${lang}">${atom}</span>`);
+        if (atom === item) {
+          if (typeof parsedInput[item][nHash] === `undefined`) {
+            parsedInput[item][nHash] = [];
+          }
+          parsedInput[item][nHash].push(lang);
           itemAdded = true;
         }
       });
     });
 
     if (!itemAdded) {
-      parsedInput.push(`<span class="unknown">${item}</span>`);
+      parsedInput[item] = `unknown`;
     }
   });
 
-  resultsHTML.html(parsedInput.join(' '));
-
-  // var regex = new RegExp(searchValue);
-  // var matches = [];
-  // Object.keys(nouns).map(hashn => {
-  //   Object.keys(nouns[hashn].root).map(lang => {
-  //     var atom = nouns[hashn].root[lang].atom;
-  //       if (atom.search(regex) != -1) {
-  //         var adjectives = [];
-  //         // matches += `<li>${f}: ${atom} (NOUN)</li>`;
-  //         Object.keys(nouns[hashn].adjs).map(hasha => {
-  //           var adj = nouns[hashn].adjs[hasha][lang];
-  //           if (typeof adj !== 'undefined') {
-  //             adjectives += `
-  //               <li>${nouns[hashn].adjs[hasha][lang].atom}</li>
-  //             `;
-  //           }
-  //         });
-  //         matches += `
-  //           <li>
-  //             ${lang}: ${atom} (NOUN)
-  //             <ul>
-  //               ${adjectives}
-  //             </ul>
-  //           </li>
-  //         `;
-  //       }
-  //   });
-  // });
-
-  // resultsUL.html(matches);
+  console.log(parsedInput);
+  // resultsHTML.html(parsedInput.join(' '));
 });
