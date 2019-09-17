92524b7d8a25be2cf162b4de18d5a74c2e146cf7
Tim Hosgood <timhosgood@googlemail.com>
2019-09-17 17:45:19 +0200

    basic searching

--- js/translate.js
+++ js/translate.js
@@ -1,22 +1,32 @@
-const nounsURL = "https://raw.githubusercontent.com/thosgood/maths-dictionary/master/json/nouns.json?token=ABWM5B7EOBETG4OS3MM4LK25RIR5K";
+const nounsURL = "https://thosgood.com/maths-dictionary/json/nouns.json";
 
-const translateInput = $( '#translateInput' );
-const resultsDiv = $( '#results' );
+const translateInput = $('#translateInput');
+const resultsDiv = $('#results');
 
 var nouns
 // TODO: this can be rewritten somehow simpler, but I can't remember how...
-$.getJSON( nounsURL, function( data ) {
+$.getJSON(nounsURL, function(data) {
     nouns = data;
 });
 
-translateInput.keyup( function() {
-    var searchValue = $( this ).val();
+translateInput.keyup(function() {
+    var searchValue = $(this).val();
 
-    if ( searchValue === '' ) {
-        resultsDiv.html( '' );
+    if (searchValue === '') {
+        resultsDiv.html('');
         return;
     }
     
-    var regex = new RegExp( searchValue, "i" );
-    console.log( Object.keys(nouns) );
+    var regex = new RegExp(searchValue, "i");
+    var matches = [];
+    Object.keys(nouns).map(e => {
+        Object.keys(nouns[e].root).map(f => {
+            var atom = nouns[e].root[f]
+            if (atom.search(regex) != -1) {
+                matches += atom;
+            }
+        });
+    });
+
+    resultsDiv.html(matches);
 });
